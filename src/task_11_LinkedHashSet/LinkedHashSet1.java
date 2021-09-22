492b53f1a684f6897bf0db4251864c11d46903a2
WoldemaK <kovtynov.vladimir@gmail.com>
2021-09-22 15:46:06 +0300

    Add

--- src/task_11_LinkedHashSet/LinkedHashSet1.java
+++ src/task_11_LinkedHashSet/LinkedHashSet1.java
@@ -1,4 +1,22 @@
 package task_11_LinkedHashSet;
 
+import java.util.LinkedHashSet;
+
 public class LinkedHashSet1 {
+    public static void main(String[] args) {
+        LinkedHashSet<Integer> linkedHashSet = new LinkedHashSet<>();
+        linkedHashSet.add(5);
+        linkedHashSet.add(3);
+        linkedHashSet.add(1);
+        linkedHashSet.add(8);
+        linkedHashSet.add(10);
+        System.out.println(linkedHashSet);
+
+        linkedHashSet.remove(8);
+        System.out.println(linkedHashSet);
+
+        System.out.println(linkedHashSet.contains(8));
+
+        System.out.println(linkedHashSet.contains(10));
+    }
 }
